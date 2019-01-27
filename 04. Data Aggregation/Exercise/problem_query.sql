f9f205dfac72e934e3511ee58cae117b40e76712
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-01-27 19:12:36 +0200

    Added new topic

---
+++ 04. Data Aggregation/Exercise/problem_query.sql
@@ -0,0 +1,147 @@
+USE Gringotts;
+GO
+
+-- Problem 01.
+SELECT COUNT(*) AS [Count]
+FROM WizzardDeposits;
+
+-- Problem 02.
+SELECT MAX(MagicWandSize)
+FROM WizzardDeposits;
+
+-- Problem 03.
+SELECT DepositGroup, 
+       MAX(MagicWandSize) AS [LongestMagicWand]
+FROM WizzardDeposits
+GROUP BY DepositGroup;
+
+-- Problem 04.
+SELECT TOP (2) DepositGroup
+FROM WizzardDeposits
+GROUP BY DepositGroup
+ORDER BY AVG(MagicWandSize);
+
+-- Problem 04.
+SELECT DepositGroup, 
+       SUM(DepositAmount) AS [TotalSum]
+FROM WizzardDeposits
+GROUP BY DepositGroup;
+
+-- Problem 05.
+SELECT DepositGroup, 
+       SUM(DepositAmount) AS [TotalSum]
+FROM WizzardDeposits
+GROUP BY DepositGroup;
+
+-- Problem 06.
+SELECT DepositGroup, 
+       SUM(DepositAmount) AS [TotalSum]
+FROM WizzardDeposits
+WHERE MagicWandCreator = 'Ollivander family'
+GROUP BY DepositGroup;
+
+-- Problem 07.
+SELECT DepositGroup, 
+       SUM(DepositAmount) AS [TotalSum]
+FROM WizzardDeposits
+WHERE MagicWandCreator = 'Ollivander family'
+GROUP BY DepositGroup
+HAVING SUM(DepositAmount) < 150000
+ORDER BY TotalSum DESC;
+
+-- Problem 08.
+SELECT DepositGroup, 
+       MagicWandCreator, 
+       MIN(DepositCharge)
+FROM WizzardDeposits
+GROUP BY DepositGroup, 
+         MagicWandCreator
+ORDER BY MagicWandCreator, 
+         DepositGroup;
+
+-- Problem 09.
+SELECT AgeGroup, 
+       COUNT(AgeGroup)
+FROM
+(
+    SELECT CASE
+               WHEN Age BETWEEN 0 AND 10
+               THEN '[0-10]'
+               WHEN Age BETWEEN 11 AND 20
+               THEN '[11-20]'
+               WHEN Age BETWEEN 21 AND 30
+               THEN '[21-30]'
+               WHEN Age BETWEEN 31 AND 40
+               THEN '[31-40]'
+               WHEN Age BETWEEN 41 AND 50
+               THEN '[41-50]'
+               WHEN Age BETWEEN 51 AND 60
+               THEN '[51-60]'
+               ELSE '[61+]'
+           END AS AgeGroup
+    FROM WizzardDeposits
+) AS AgeTable
+GROUP BY AgeGroup;
+
+-- Problem 10.
+SELECT LEFT(FirstName, 1) AS Letter
+FROM WizzardDeposits
+WHERE DepositGroup = 'Troll Chest'
+GROUP BY LEFT(FirstName, 1);
+
+-- Problem 11.
+SELECT DepositGroup, 
+       IsDepositExpired, 
+       AVG(DepositInterest) AverageInterest
+FROM WizzardDeposits
+WHERE DepositStartDate > '01-01-1985'
+GROUP BY DepositGroup, 
+         IsDepositExpired
+ORDER BY DepositGroup DESC, 
+         IsDepositExpired;
+
+-- Problem 12.
+
+USE SoftUni;
+-- Problem 13.
+SELECT DepartmentID, 
+       SUM(Salary) [TotalSalary]
+FROM Employees
+GROUP BY DepartmentID;
+
+-- Problem 14.
+SELECT DepartmentID, 
+       MIN(Salary) [TotalSalary]
+FROM Employees
+WHERE DepartmentID IN(2, 5, 7)
+AND YEAR(HireDate) >= 2000
+GROUP BY DepartmentID;
+
+-- Problem 15.
+SELECT *
+INTO NewTB
+FROM Employees
+WHERE Salary > 30000
+      AND ManagerID <> 42;
+UPDATE NewTB
+  SET 
+      Salary+=5000
+WHERE DepartmentID = 1;
+SELECT DepartmentID, 
+       AVG(Salary)
+FROM NewTB
+GROUP BY DepartmentID;
+
+-- Problem 16.
+SELECT DepartmentID, 
+       MAX(Salary) [MaxSalary]
+FROM Employees
+GROUP BY DepartmentID
+HAVING MAX(Salary) NOT BETWEEN 30000 AND 70000;
+
+-- Problem 17.
+SELECT COUNT(*)
+FROM Employees
+WHERE ManagerID IS NULL;
+
+-- Problem 19.

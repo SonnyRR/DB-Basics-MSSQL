fe93703b3d1c4c98ad6eb8386de9d42be7fbbd7e
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-02-12 18:07:54 +0200

    Refactoring.

--- 07-Programability and Transaction/Exercise/problem_query.sql
+++ 07-Programability and Transaction/Exercise/problem_query.sql
@@ -174,3 +174,13 @@ AS
 	ORDER BY FirstName, LastName
 
 EXEC usp_GetHoldersWithBalanceHigherThan 10000
+
+-- 11. --
+CREATE FUNCTION ufn_CalculateFutureValue(@sum DECIMAL(15,2), @yearlyInterestRate FLOAT, @numberOfYears INT)
+RETURNS DECIMAL(15,4)
+BEGIN
+		DECLARE @result DECIMAL(15,4) = @sum * (POWER(1 + @yearlyInterestRate, @numberOfYears));
+		RETURN @result
+END
+
+SELECT EXEC dbo.ufn_CalculateFutureValue 1000, 0.1, 5
